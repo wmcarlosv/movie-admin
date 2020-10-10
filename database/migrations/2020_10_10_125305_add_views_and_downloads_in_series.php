4caf751e80acdb5a2d55a01c09eab5ce03f5f50f
= <=>
2020-10-10 08:53:41 -0400

    add views and downloads

---
+++ database/migrations/2020_10_10_125305_add_views_and_downloads_in_series.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddViewsAndDownloadsInSeries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('series', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('series', function (Blueprint $table) {
+            $table->integer('views')->nullable()->default(0);
+            $table->integer('downloads')->nullable()->default(0);
+        });
+    }
+}
